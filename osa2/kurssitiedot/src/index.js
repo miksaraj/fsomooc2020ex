d7dc74d9ce574eae5d120010797425db2145f7dc
Mikko Rajakangas <mikko.rajakangas@visma.com>
2020-07-06 23:51:08 +0300

    init 2b

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -2,4 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const courses = [
+    {
+        id: 1,
+        name: 'Half Stack application development',
+        parts: [
+            { id: 1, name: 'Fundamentals of React', exercises: 10 },
+            { id: 2, name: 'Using props to pass data', exercises: 7 },
+            { id: 3, name: 'State of a component', exercises: 14 },
+            { id: 4, name: 'Redux', exercises: 11 }
+        ]
+    },
+    {
+        id: 2,
+        name: 'Node.js',
+        parts: [
+            { id: 1, name: 'Routing', exercises: 3 },
+            { id: 2, name: 'Middlewares', exercises: 7 },
+        ]
+    }
+]
+
+ReactDOM.render(<App courses={courses} />, document.getElementById('root'))
